43cb3d0e4a5c78197eaf7b14a6848781f65164ce
YashLangote <yashlangote97@gmail.com>
2026-01-02 18:55:40 +0530

    Update App.java

--- src/main/com/EagleHead/app/App.java
+++ src/main/com/EagleHead/app/App.java
@@ -5,7 +5,7 @@ package com.EagleHead.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World2!";
 
     public App() {}
 
